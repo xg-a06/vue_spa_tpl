3aface327db2917823b8cb9de58e34019007cf0c
xg-a06 <xg_jser@163.com>
2019-05-20 01:44:48 +0800

    添加babel env 选项支持

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ const presets = [
 ]
 const plugins = [
   ['@babel/plugin-transform-runtime', { corejs: 2 }],
+  {{#if es6Env==='STAGE1'}}
   '@babel/plugin-proposal-export-default-from', // Stage 1
   '@babel/plugin-proposal-logical-assignment-operators',
   ['@babel/plugin-proposal-optional-chaining', { loose: false }],
@@ -25,7 +26,24 @@ const plugins = [
   ['@babel/plugin-proposal-class-properties', { loose: false }],
   '@babel/plugin-proposal-json-strings',
   '@babel/plugin-proposal-object-rest-spread',
-  // '@babel/plugin-syntax-jsx',
+  {{else if es6Env==='STAGE2'}}
+  ['@babel/plugin-proposal-decorators', { legacy: true }], // Stage 2
+  '@babel/plugin-proposal-function-sent',
+  '@babel/plugin-proposal-export-namespace-from',
+  '@babel/plugin-proposal-numeric-separator',
+  '@babel/plugin-proposal-throw-expressions',
+  '@babel/plugin-syntax-dynamic-import', // Stage 3
+  '@babel/plugin-syntax-import-meta',
+  ['@babel/plugin-proposal-class-properties', { loose: false }],
+  '@babel/plugin-proposal-json-strings',
+  '@babel/plugin-proposal-object-rest-spread',
+  {{else}}
+  '@babel/plugin-syntax-dynamic-import', // Stage 3
+  '@babel/plugin-syntax-import-meta',
+  ['@babel/plugin-proposal-class-properties', { loose: false }],
+  '@babel/plugin-proposal-json-strings',
+  '@babel/plugin-proposal-object-rest-spread',
+  {{/if}}
   'transform-vue-jsx'
   // [
   //   'component',
