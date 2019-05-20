b4280108ab85967fba4f20d6bad69a23923abcd4
xg-a06 <xg_jser@163.com>
2019-05-21 01:09:31 +0800

    去掉无用文件,修复压缩问题

--- build/webpack.common.conf.js
+++ build/webpack.common.conf.js
@@ -86,7 +86,6 @@ module.exports = () => {
         },
         minimizer: [
           new ParallelUglifyPlugin({
-            cacheDir: '.cache/',
             include: [/[\\/]src[\\/]/],
             uglifyJS: {
               output: {
